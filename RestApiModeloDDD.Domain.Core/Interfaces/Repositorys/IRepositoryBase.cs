086dafeee544a79697cdbd44a7944873e9dabf76
andrywmarcelo.lima@gmail.com <andrywmarcelo.lima@gmail.com>
2022-04-26 21:40:44 -0300

    Feat: Iniciação do projeto ApiRest com DDD

---
+++ RestApiModeloDDD.Domain.Core/Interfaces/Repositorys/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestApiModeloDDD.Domain.Core.Interfaces.Repositorys
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void add(TEntity obj);
+        void Update(TEntity oj);
+        void remove(TEntity obj);
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id); 
+    }
+}
